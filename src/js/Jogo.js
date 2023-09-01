c9bfc093765dd31889def0af0fdee01d4d1c47a1
Vinicius Araujo <61100469+ViniAraujo99@users.noreply.github.com>
2023-08-31 22:40:10 -0300

    fixing performance

--- src/js/Jogo.js
+++ src/js/Jogo.js
@@ -113,8 +113,8 @@ function criaCards(_dificuldade) {
 
         img.src = `./src/img/${arraySort[i]}.png`;
         img.alt = `${arraySort[i]}`;
-        img.width = '150px';
-        img.height = '150px';
+        img.width = 150 + 'px';
+        img.height = 150 + 'px';
         divBackView.innerHTML = "?";
     }
 }
